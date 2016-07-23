5597410727ca7a885c71c9a0fb4908d1c678112a
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-07-23 11:36:12 -0500

    feat(app): Scaffold base app structure and add layout styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
@@ -8,7 +9,12 @@ import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MdIcon],
+  directives: [
+    MD_TOOLBAR_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MD_BUTTON_DIRECTIVES,
+    MdIcon
+  ],
   providers: [MdIconRegistry]
 })
 export class AppComponent {
