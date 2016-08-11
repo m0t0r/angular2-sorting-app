d8b4ec735208870fca5c998e8e522234eb733719
m0t0r <tkachenko.vitaly.job@gmail.com>
2016-08-10 19:04:32 -0500

    refactor(app): Create app module and refactor bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,4 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
-import { disableDeprecatedForms, provideForms} from '@angular/forms';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { CommandService } from './app/common/services/command/command.service';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  disableDeprecatedForms(),
-  provideForms(),
-  CommandService
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
